c62024302b0d3c8ce3b4e18cd65065aaaee390e7
redivus <adhithi@redivus.com>
2018-10-08 10:09:46 -0500

    Updating store

--- ReduxInRN/src/store/index.js
+++ ReduxInRN/src/store/index.js
@@ -1,6 +1,6 @@
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 import rootReducer from '../reducers'
-import todos from '../reducers/todos'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
 
 export default store = createStore(rootReducer)
